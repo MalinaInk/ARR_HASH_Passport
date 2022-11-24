c0fcf60c6bedee621d7b75d5f21e6328f5df6703
mo <nataleshen.ko@yandex.ru>
2022-11-24 16:04:54 +0300

    commit - 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+import static java.time.LocalDate.of;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static Set<Passport> passports1 = new HashSet<>();
+        public static void main(String[] args) {
+
+        Passport passport1 = new Passport(12967889, "Лермонтов", "Михаил", "Юрьевич", LocalDate.of(1991, 3, 12));
+        Passport passport2 = new Passport(27156734, "Пушкин", "Александр", "Сергеевич", LocalDate.of(1984, 12, 3));
+        Passport passport3 = new Passport(92579264, "Достоевский", "Федор", "Михайлович", LocalDate.of(1997, 9, 10));
+
+      passports1.add(passport1);
+      passports1.add(passport2);
+            System.out.println(passports1);
+
+
+
+
     }
 }
